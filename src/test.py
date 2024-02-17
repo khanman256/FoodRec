5745b381420a7efaab705b6d5ae4955fb8ca97f1
srijal30 <srijal30@stuy.edu>
2024-02-17 16:44:42 -0500

    work session

---
+++ src/test.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Station():
+    pass
+
+# Dining Hall
+class DiningHall():
+    stations: List[Station] = []
+
+    def __init__(self, name):
+
+    
+
+
+# Station
+
+DiningHall("sltlsdjfd", "dsljs;lkfj")
+
+# Menu Item
